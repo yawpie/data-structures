05379840f5bef377ebd66f183e10adee7f67c80e
Muhammad Rafi <iamyawpie@protonmail.com>
2023-11-01 15:13:11 +0800

    small changes

--- LinkedList/Main.java
+++ LinkedList/Main.java
@@ -1,6 +1,11 @@
 package LinkedList;
 
-public class Driver {
+/**
+ * The class Main is not a part of the data structure
+ * but is a part for performing test on the data structures in order to make
+ * sure it is correct
+ */
+public class Main {
 
     public static void main(String[] args) {
         SingleList single = new SingleList();
